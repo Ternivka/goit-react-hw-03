a89b712f0e823edf4c480af72154c3060b262a41
yeah <154704827+Ternivka@users.noreply.github.com>
2025-01-12 15:58:28 +0100

    ContactList, Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,2 +1,22 @@
-const Contact = () => {};
+import s from "./Contact.module.css";
+import { IoPerson, IoCall } from "react-icons/io5";
+
+const Contact = ({ name, number, id }) => {
+  return (
+    <li key={id} className={s.item}>
+      <div>
+        <p>
+          <IoPerson size={20} className={s.icon} />
+          {name}
+        </p>
+        <p>
+          <IoCall size={20} className={s.icon} />
+          {number}
+        </p>
+      </div>
+
+      <button>Delete</button>
+    </li>
+  );
+};
 export default Contact;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,2 +1,14 @@
-const ContactList = () => {};
+import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css";
+const ContactList = ({ contactArr }) => {
+  return (
+    <div>
+      <ul className={s.list}>
+        {contactArr.map(({ id, name, number }) => (
+          <Contact key={id} id={id} name={name} number={number} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default ContactList;
